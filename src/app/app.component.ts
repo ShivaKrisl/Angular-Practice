441e6875ead1264885e41e0263fcdc15354fd10d
ShivaKrsl <shivakrishnabeeraboina@gmail.com>
2025-02-20 11:14:27 +0530

    Added Header Component learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   title = 'first-angular-app';
