6675d7cf79a32bc4cb0ea78af41af232aa646a8e
ShivaKrsl <shivakrishnabeeraboina@gmail.com>
2025-02-23 19:59:57 +0530

    added user component input signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy_users';
 
 @Component({
@@ -8,12 +8,15 @@ import { DUMMY_USERS } from '../dummy_users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
 
-  get ImagePath(): string {
-    return `assets/users/${this.avatar}`;
-  }
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  ImagePath = computed(() => {
+    return `assets/users/${this.avatar()}`;
+  });
 
   onSelectUser(): void {}
 }
