3f0c4bb5713a66fe9b71aa1c280c0123108747c2
ShivaKrsl <shivakrishnabeeraboina@gmail.com>
2025-02-24 14:46:22 +0530

    Added Tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy_users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
 
+  selectedUserId: string = 'u1';
+
+  get SelectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
   // event handler
   selectUserTasks(id: string) {
-    console.log('User ID:', id);
+    this.selectedUserId = id;
   }
 }
