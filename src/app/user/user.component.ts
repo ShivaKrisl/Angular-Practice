de1b62c2b376084593da6225c51153617b6e851c
ShivaKrsl <shivakrishnabeeraboina@gmail.com>
2025-02-24 22:41:27 +0530

    Added Object as inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,6 +9,12 @@ import {
 } from '@angular/core';
 import { DUMMY_USERS } from '../dummy_users';
 
+interface IUser {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -18,19 +24,17 @@ import { DUMMY_USERS } from '../dummy_users';
 export class UserComponent {
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
-  id = input.required<string>();
-  avatar = input.required<string>();
-  name = input.required<string>();
+  user = input.required<IUser>();
 
   // custom event
   // @Output() select = new EventEmitter();
   select = output<string>();
 
   ImagePath = computed(() => {
-    return `assets/users/${this.avatar()}`;
+    return `assets/users/${this.user().avatar}`;
   });
 
   onSelectUser(): void {
-    this.select.emit(this.id()); // pass user id to parent component
+    this.select.emit(this.user().id); // pass user id to parent component
   }
 }
