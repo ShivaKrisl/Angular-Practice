eb78e8f7f62dc8c13600653494f35f2cef820fc4
ShivaKrsl <shivakrishnabeeraboina@gmail.com>
2025-02-26 22:38:48 +0530

    Added ngModel two-way binding

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,13 +1,18 @@
 import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   cancel = output();
+
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDate: string = '';
 
   onCancel() {
     this.cancel.emit();
