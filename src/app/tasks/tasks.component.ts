680354ec6c50b98911c34328ac29a0d7ee99b900
ShivaKrsl <shivakrishnabeeraboina@gmail.com>
2025-02-26 18:19:09 +0530

    Added Add task templates

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
 import { dummyTasks } from '../dummy_tasks';
 import { ITask } from '../ITask';
+import { NewTaskComponent } from '../new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -15,11 +16,21 @@ export class TasksComponent {
 
   tasks: Array<ITask> = dummyTasks;
 
+  isAddTaskVisible: boolean = false;
+
   get getSelectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userid());
   }
 
   completeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onAddTask() {
+    this.isAddTaskVisible = true;
+  }
+
+  onClearAddTask() {
+    this.isAddTaskVisible = false;
   }
 }
